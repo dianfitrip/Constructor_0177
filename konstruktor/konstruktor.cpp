ffa88dcac9bf621aeab4838c67746868d6093405
dianfitrip <dianfitri683@gmail.com>
2024-05-14 14:47:45 +0700

    membuat class mahasiswa,  dgn 2 member ada konstruktor di dlm class

---
+++ konstruktor/konstruktor.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa {
+private:
+	int nim;
+	string nama;
+public:
+	Mahasiswa() {
+		nim = 0;
+		nama = "";
+	}
+	Mahasiswa(int iNim) {
+		nim = iNim;
+	}
+	Mahasiswa(string);
+	Mahasiswa(int iNim, string iNama);
+	void cetak();
+};
+
+
+
