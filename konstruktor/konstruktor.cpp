3490bb717adc43c91aeadc1c18ae89b6c9488417
dianfitrip <dianfitri683@gmail.com>
2024-05-14 14:52:39 +0700

    konstruktor yg lain yg berada diluar, memanggil dan mencetak yang sesui tipe data nya

--- konstruktor/konstruktor.cpp
+++ konstruktor/konstruktor.cpp
@@ -21,3 +21,32 @@ public:
 
 
 
+Mahasiswa::Mahasiswa(string iNama) {
+	nim = 0;
+	nama = iNama;
+}
+
+Mahasiswa::Mahasiswa(int iNim, string iNama) {
+	nim = iNim;
+	nama = iNama;
+}
+
+void Mahasiswa::cetak() {
+	cout << endl << "Nim = " << nim << endl;
+	cout << "Nama = " << nama << endl;
+}
+
+int main() {
+	Mahasiswa mhs1;
+	Mahasiswa mhs2(99);
+	Mahasiswa mhs3("Mikacangggg");
+	Mahasiswa mhs4(99, "Mikacangggg");
+
+	mhs1.cetak();
+	mhs2.cetak();
+	mhs3.cetak();
+	mhs4.cetak();
+
+	system("pause");
+	return 0;
+}
