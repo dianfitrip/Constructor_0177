7195612b7e3e7472c78df06be55bcac5bef2a126
dianfitrip <dianfitri683@gmail.com>
2024-05-14 14:41:32 +0700

    membuat variabel statis, memanggil dan mencetaknya.lalu dari mhs3 nim brubh,

--- constructorWfunction/constructorWfunction.cpp
+++ constructorWfunction/constructorWfunction.cpp
@@ -18,3 +18,32 @@ public:
 	Mahasiswa(string pnama) :nama(pnama) { setID(); }
 };
 
+int Mahasiswa::nim = 0;
+
+void Mahasiswa::setID() {
+	id = ++nim;
+}
+
+void Mahasiswa::printAll() {
+	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
+
+int main() {
+	Mahasiswa mhs1("Sri Dadi");
+	Mahasiswa mhs2("Budi Jatmiko");
+	Mahasiswa::setNim(9); //mengakses nim melalui static member function "setNim"
+	Mahasiswa mhs3("Andi Janu");
+	Mahasiswa mhs4("Joko Wahono");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+
+	cout << "akses dari luar object = " << Mahasiswa::getNim() << endl; //mengakses nim melalui static member function "getNim"
+	system("pause");
+
+	return 0;
+}
