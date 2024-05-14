88ff2604df55c4eaf9dbf30d3fa79fdb2dc3f029
dianfitrip <dianfitri683@gmail.com>
2024-05-14 14:27:33 +0700

    membuat class mahasiswa

--- constructor_destructor/constructor_destructor.cpp
+++ constructor_destructor/constructor_destructor.cpp
@@ -2,3 +2,17 @@
 #include <string>
 using namespace std;
 
+class Mahasiswa {
+public:
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+
+	Mahasiswa(string pnama) :nama(pnama) {
+		setID();
+	}
+};
+
